53f8c734c389acdd23efb501a0ba6eafb8d96c42
gleb7499 <164905074+gleb7499@users.noreply.github.com>
2024-09-23 19:57:58 +0300

    Some refactoring

--- laba_1/app/src/main/java/com/example/laba_1/MainActivity.java
+++ laba_1/app/src/main/java/com/example/laba_1/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.laba_1;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,10 +21,21 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        ViewCompat.setOnApplyWindowInsetsListener(bottomNavigationView, (v, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            mlp.leftMargin = insets.left;
+            mlp.bottomMargin = insets.bottom;
+            mlp.rightMargin = insets.right;
+            v.setLayoutParams(mlp);
+            return WindowInsetsCompat.CONSUMED;
+        });
 
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.search);
@@ -29,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     SearchFragment searchFragment = new SearchFragment();
 
     // Переключение экранов нижней панели
+    @SuppressLint("NonConstantResourceId")
     @Override
     public boolean
     onNavigationItemSelected(@NonNull MenuItem item) {
--- laba_1/app/src/main/java/com/example/laba_1/SearchFragment.java
+++ laba_1/app/src/main/java/com/example/laba_1/SearchFragment.java
@@ -1,27 +1,42 @@
 package com.example.laba_1;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-
-import androidx.constraintlayout.motion.widget.MotionLayout;
-import androidx.fragment.app.Fragment;
-
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONArray;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -33,15 +48,13 @@ import java.net.URL;
 
 public class SearchFragment extends Fragment {
 
-    private MotionLayout motionLayout;
     private ProgressBar progressBar;
-    private Button button;
     private EditText editText;
     private ImageView imageView;
 
-    private boolean flag_for_button = false;
-
-    private String rawUrl;
+    private String ImageURL = "";
+    private String pageURL = "";
+    private String Title = "";
 
     public SearchFragment() {
         // Required empty public constructor
@@ -53,41 +66,10 @@ public class SearchFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_search, container, false);
 
-        motionLayout = view.findViewById(R.id.motionLayout);
         progressBar = view.findViewById(R.id.progressBar);
-        button = view.findViewById(R.id.button_for_searching);
+        Button button = view.findViewById(R.id.button_for_searching);
         editText = view.findViewById(R.id.input_for_searching);
         imageView = view.findViewById(R.id.imageView);
-
-        motionLayout.setTransitionListener(new MotionLayout.TransitionListener() {
-            @Override
-            public void onTransitionStarted(MotionLayout motionLayout, int startId, int endId) {
-                // Анимация началась
-            }
-
-            @Override
-            public void onTransitionChange(MotionLayout motionLayout, int startId, int endId, float progress) {
-                // Анимация в процессе
-            }
-
-            @Override
-            public void onTransitionCompleted(MotionLayout motionLayout, int currentId) {
-                // Анимация завершена
-                progressBar.clearAnimation();
-                progressBar.setVisibility(View.INVISIBLE);
-                if (rawUrl != null) {
-                    Glide.with(SearchFragment.this)
-                            .load(rawUrl)
-                            .into(imageView);
-                }
-                imageView.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void onTransitionTrigger(MotionLayout motionLayout, int triggerId, boolean positive, float progress) {
-                // Триггер анимации
-            }
-        });
 
 
         // Установить прослушку
@@ -98,22 +80,109 @@ public class SearchFragment extends Fragment {
             }
         });
 
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(v);
+            }
+        });
+
         return view;
+    }
+
+    private void showPopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(getContext(), view);
+        popupMenu.getMenuInflater().inflate(R.menu.image_menu, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (!ImageURL.isEmpty()) {
+                    switch (item.getItemId()) {
+                        case R.id.download:
+                            // Логика для скачивания изображения
+                            downloadImage(ImageURL);
+                            Toast.makeText(getContext(), "Загрузка изображения", Toast.LENGTH_SHORT).show();
+                            return true;
+                        case R.id.range:
+                            // Логика для оценки изображения
+                            showRatingDialog();
+                            return true;
+                        case R.id.original:
+                            // Переход на сайт изображения
+                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(pageURL));
+                            requireContext().startActivity(intent);
+                        default:
+                            return false;
+                    }
+                }
+                return false;
+            }
+        });
+
+        popupMenu.show();
+    }
+
+    private void showRatingDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_rate_image, null);
+        builder.setView(dialogView);
+
+        RatingBar ratingBar = dialogView.findViewById(R.id.ratingBar);
+        builder.setPositiveButton("Оценить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                float rating = ratingBar.getRating();
+                // Логика для обработки рейтинга
+                Toast.makeText(getContext(), "Вы оценили изображение на " + rating + " звезд", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void downloadImage(String url) {
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
+        request.setTitle(Title);
+        request.allowScanningByMediaScanner();
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, Title + ".jpg");
+
+        DownloadManager manager = (DownloadManager) requireActivity().getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
     }
 
     public void ButtonClick(View view) {
         String query = editText.getText().toString();
         if (!query.isEmpty()) {
-            flag_for_button = true;
             imageView.setVisibility(View.INVISIBLE);
             progressBar.setVisibility(View.VISIBLE);
-            motionLayout.transitionToEnd();
 
             String accessKey = BuildConfig.UNSPLASH_ACCESS_KEY;
-            String url = "https://api.unsplash.com/search/photos?query=" + query + "&per_page=1&client_id=" + accessKey;
+            String url = "https://pixabay.com/api/?key=" + accessKey + "&q=" + query.replaceAll(" ", "+") + " &image_type=photo&pretty=true&lang=ru&orientation=vertical&per_page=3";
 
             new FetchImageTask().execute(url);
+
+            Title = query;
         }
+    }
+
+    void showError(String errorMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setMessage(errorMessage)
+                .setPositiveButton("OK", null)
+                .show();
     }
 
     private class FetchImageTask extends AsyncTask<String, Void, String> {
@@ -125,16 +194,33 @@ public class SearchFragment extends Fragment {
                 jsonString = getJsonFromUrl(urls[0]);
                 Log.d("FetchImageTask", "JSON received: " + jsonString);
                 JSONObject jsonObject = new JSONObject(jsonString);
-                JSONArray results = jsonObject.getJSONArray("results");
+                JSONArray results = jsonObject.getJSONArray("hits");
+                if (results.length() == 0) {
+                    ImageURL = "";
+                    requireActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            showError("Ничего не найдено");
+                            progressBar.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    return null;
+                }
                 JSONObject result = results.getJSONObject(0);
-                JSONObject urlsObject = result.getJSONObject("urls");
-                String rawUrl = urlsObject.getString("raw");
-                Log.d("FetchImageTask", "Image URL: " + rawUrl);
-                return rawUrl;
+                ImageURL = result.getString("largeImageURL");
+                pageURL = result.getString("pageURL");
+                Log.d("FetchImageTask", "Image URL: " + ImageURL);
+                return ImageURL;
             } catch (IOException | JSONException e) {
-                e.printStackTrace();
                 Log.e("FetchImageTask", "Error: " + e.getMessage());
-                return null;
+                requireActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.INVISIBLE);
+                        showError(e.getMessage());
+                    }
+                });
+                return "Error";
             }
         }
 
@@ -142,10 +228,30 @@ public class SearchFragment extends Fragment {
         protected void onPostExecute(String rawUrl) {
             super.onPostExecute(rawUrl);
             Log.d("FetchImageTask", "onPostExecute: " + rawUrl);
-            SearchFragment.this.rawUrl = rawUrl;
+            if (rawUrl != null && !rawUrl.equals("No result") && !rawUrl.equals("Error")) {
+                Glide.with(requireActivity()).load(rawUrl).listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        Log.d("Glide", "onLoadFailed");
+                        showError("Ошибка загрузки изображения");
+                        progressBar.setVisibility(View.INVISIBLE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        Log.d("Glide", "onResourceReady");
+                        imageView.setVisibility(View.VISIBLE);
+                        progressBar.setVisibility(View.INVISIBLE);
+                        return false;
+                    }
+
+                }).transition(DrawableTransitionOptions.withCrossFade(400)).into(imageView);
+
+            }
+
         }
     }
-
 
     public String getJsonFromUrl(String urlString) throws IOException {
         URL url = new URL(urlString);
