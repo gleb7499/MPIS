f34e283994ce2a9f4b50a9e1301112a3d4d24fe0
gleb7499 <164905074+gleb7499@users.noreply.github.com>
2024-09-26 18:32:45 +0300

    Добавлены все три активити, корректно работает логика ввода и обмена данными между активностями.

--- laba_3/app/src/main/java/com/example/laba_3/FirstActivity.java
+++ laba_3/app/src/main/java/com/example/laba_3/FirstActivity.java
@@ -1,16 +1,25 @@
 package com.example.laba_3;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class FirstActivity extends AppCompatActivity {
+
+    private Button buttonCallTaxi;
+    private TextView textViewRoute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +32,68 @@ public class FirstActivity extends AppCompatActivity {
             return insets;
         });
 
-        Intent intent = getIntent();
+        Intent intent_1 = getIntent();
+        Intent intent_2 = new Intent("com.example.laba_3.SecondActivity");
 
         TextView textViewName = findViewById(R.id.textViewName);
         TextView textViewPhone = findViewById(R.id.textViewPhone);
-        textViewName.setText(getString(R.string.name_and_lastname, intent.getStringExtra("name"), intent.getStringExtra("lastname")));
-        textViewPhone.setText(intent.getStringExtra("phone"));
+        textViewRoute = findViewById(R.id.textViewRoute);
 
+        textViewName.setText(getString(R.string.name_and_lastname, intent_1.getStringExtra("name"), intent_1.getStringExtra("lastname")));
+        textViewPhone.setText(intent_1.getStringExtra("phone"));
 
+        Button buttonSetPath = findViewById(R.id.buttonSetPath);
+        buttonCallTaxi = findViewById(R.id.buttonCallTaxi);
+
+        buttonSetPath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(intent_2, 3);
+            }
+        });
+
+        buttonCallTaxi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String route = textViewRoute.getText().toString();
+                if (!route.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(FirstActivity.this);
+                    builder.setMessage("Такси успешно заказано!")
+                            .setPositiveButton("Спасибо", null)
+                            .show();
+                }
+                else {
+                    Toast.makeText(FirstActivity.this, "Укажите маршрут!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 3 && resultCode == RESULT_OK && data != null) {
+            String city = data.getStringExtra("city");
+            String street = data.getStringExtra("street");
+            String house = data.getStringExtra("house");
+            String corpus = data.getStringExtra("corpus");
+            String door = data.getStringExtra("door");
+            String flat = data.getStringExtra("flat");
+
+            String route = "г. " + city + ", ул. " + street + ", дом " + house;
+            if (corpus != null && !corpus.isEmpty()) {
+                route += ", корпус " + corpus;
+            }
+            if (door != null && !door.isEmpty()) {
+                route += ", подъезд " + door;
+            }
+            if (flat != null && !flat.isEmpty()) {
+                route += ", кв. " + flat;
+            }
+            textViewRoute.setText(route);
+
+            buttonCallTaxi.setEnabled(true);
+            buttonCallTaxi.setAlpha(1.0f);
+        }
     }
 }
--- laba_3/app/src/main/java/com/example/laba_3/SecondActivity.java
+++ laba_3/app/src/main/java/com/example/laba_3/SecondActivity.java
@@ -1,6 +1,9 @@
 package com.example.laba_3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +24,36 @@ public class SecondActivity extends AppCompatActivity {
             return insets;
         });
 
+        EditText editTextCity = findViewById(R.id.editTextCity);
+        EditText editTextStreet = findViewById(R.id.editTextStreet);
+        EditText editTextHouse = findViewById(R.id.editTextHouse);
+        EditText editTextCorpus = findViewById(R.id.editTextCorpus);
+        EditText editTextDoor = findViewById(R.id.editTextDoor);
+        EditText editTextFlat = findViewById(R.id.editTextFlat);
 
+        Intent intent = new Intent();
+
+        findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String city = editTextCity.getText().toString();
+                final String street = editTextStreet.getText().toString();
+                final String house = editTextHouse.getText().toString();
+                final String corpus = editTextCorpus.getText().toString();
+                final String door = editTextDoor.getText().toString();
+                final String flat = editTextFlat.getText().toString();
+
+                if (!city.isEmpty() && !street.isEmpty() && !house.isEmpty()) {
+                    intent.putExtra("city", city);
+                    intent.putExtra("street", street);
+                    intent.putExtra("house", house);
+                    intent.putExtra("corpus", corpus);
+                    intent.putExtra("door", door);
+                    intent.putExtra("flat", flat);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            }
+        });
     }
 }
