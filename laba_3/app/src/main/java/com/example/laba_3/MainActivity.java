38fba6db8a6c47726d53b8bfa802c3c2aa99f087
gleb7499 <164905074+gleb7499@users.noreply.github.com>
2024-09-26 14:44:23 +0300

    Добавлена логика показа второго активити, передача данных из первого во второе, добавлено 3 активити, без логики

--- laba_3/app/src/main/java/com/example/laba_3/MainActivity.java
+++ laba_3/app/src/main/java/com/example/laba_3/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.laba_3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,30 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText editTextName, editTextLastname, editTextPhone;
+
+        editTextName = findViewById(R.id.editTextName);
+        editTextLastname = findViewById(R.id.editTextLastname);
+        editTextPhone = findViewById(R.id.editTextPhone);
+
+        Intent intent = new Intent(this, FirstActivity.class);
+
+        findViewById(R.id.buttonRegistration).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String name = editTextName.getText().toString();
+                final String lastname = editTextLastname.getText().toString();
+                final String phone = editTextPhone.getText().toString();
+
+                if (!name.isEmpty() && !lastname.isEmpty() && !phone.isEmpty()) {
+                    intent.putExtra("name", name);
+                    intent.putExtra("lastname", lastname);
+                    intent.putExtra("phone", phone);
+                    startActivity(intent);
+                }
+            }
         });
     }
 }
--- laba_3/app/src/main/java/com/example/laba_3/SecondActivity.java
+++ laba_3/app/src/main/java/com/example/laba_3/SecondActivity.java
@@ -1,8 +1,6 @@
 package com.example.laba_3;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +8,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class FirstActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_first);
+        setContentView(R.layout.activity_second);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Intent intent = getIntent();
-
-        TextView textViewName = findViewById(R.id.textViewName);
-        TextView textViewPhone = findViewById(R.id.textViewPhone);
-        textViewName.setText(getString(R.string.name_and_lastname, intent.getStringExtra("name"), intent.getStringExtra("lastname")));
-        textViewPhone.setText(intent.getStringExtra("phone"));
 
 
     }
