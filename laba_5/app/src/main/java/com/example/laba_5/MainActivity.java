1a8ad162224f7d5b4e84ca3a02b2f93b5926d765
gleb7499 <164905074+gleb7499@users.noreply.github.com>
2024-10-14 17:54:14 +0300

    Использован ViewPager2 и TabLayout.
    Создан класс ViewPagerAdapter в соответствующем файле.
    Фрагменты отображаются и переключаются правильно. Фрагменты пусты.

--- laba_5/app/src/main/java/com/example/laba_5/MainActivity.java
+++ laba_5/app/src/main/java/com/example/laba_5/MainActivity.java
@@ -1,24 +1,61 @@
 package com.example.laba_5;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.laba_5.Fragments.*;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private void setMargins(View view) {
+        ViewCompat.setOnApplyWindowInsetsListener(view, (v, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            mlp.leftMargin = insets.left;
+            mlp.bottomMargin = insets.bottom;
+            mlp.rightMargin = insets.right;
+            mlp.topMargin = insets.top;
+            v.setLayoutParams(mlp);
+            return WindowInsetsCompat.CONSUMED;
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        ViewPager2 viewPager = findViewById(R.id.ViewPager);
+
+        setMargins(tabLayout);
+
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(new FragmentAdd());
+        fragments.add(new FragmentDel());
+        fragments.add(new FragmentShow());
+        fragments.add(new FragmentUpdate());
+
+        ViewPagerAdapter pagerAdapter = new ViewPagerAdapter(this, fragments);
+        viewPager.setAdapter(pagerAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            tab.setText("Вкладка " + (position + 1));
+        }).attach();
     }
 }
