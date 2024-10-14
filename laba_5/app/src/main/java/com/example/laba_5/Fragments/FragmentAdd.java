1576350f26dc43582c961a9218c227df80a4e79a
gleb7499 <164905074+gleb7499@users.noreply.github.com>
2024-10-14 11:15:42 +0300

    Инициализированы фрагменты

---
+++ laba_5/app/src/main/java/com/example/laba_5/Fragments/FragmentAdd.java
@@ -0,0 +1,26 @@
+package com.example.laba_5.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.laba_5.R;
+
+public class FragmentAdd extends Fragment {
+
+    public FragmentAdd() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add, container, false);
+
+
+
+        return view;
+    }
+}
